0dc5cb32da6ac08afb50e34f999c58e19e7c5b74
Sandeepshinde630 <sandeepshinde630@gmail.com>
2021-09-14 17:07:49 +0530

    Add project files.

---
+++ AddressBookADO/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace AddressBookADO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To AddressBook Data Base Program!");
+            AddressBookSQLRepo repo = new AddressBookSQLRepo();
+            AddressBookModel enter = new AddressBookModel();
+            repo.GetDetails();
+        }
+    }
+}
